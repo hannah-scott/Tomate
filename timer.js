65b228dff0534dcab1a3b3194851532289f8f6c1
Hannah Scott <Hannah.Scott@flgroup.co.uk>
2020-06-23 13:33:30 +0100

    added tally system

--- timer.js
+++ timer.js
@@ -1,78 +1,94 @@
+class Timer {
+    constructor(row) {
+        this.row = row;
+        this.id = row.getAttribute("id");
+        this.minutes = row.getElementsByClassName("minutes")[0]
+            .innerHTML;
+        this.seconds = row.getElementsByClassName("seconds")[0]
+            .innerHTML;
+        this.button = row.getElementsByClassName("go")[0];
+        this.going = false;
+        this.interval = null;
+        this.done = 0;
+        this.tally = document.getElementById("tally");
+        
+        // Update document elements
+        this.updateTime();
+        
+    }
+    
+    oneSecond() {
+        if (this.going == true) {
+            this.seconds -= 1;
+            if (this.seconds < 0) {
+                this.minutes -= 1;
+                this.seconds = 59;
+            }
+        }
+    }
+    
+    checkIfDone() {
+        if (this.seconds == 0 && this.minutes == 0) {
+            document.title = "Tomate: Done!";
+            alert("Done!");
+            document.title = "Tomate";
 
-    class Timer {
-        constructor(row) {
-            this.row = row;
-            this.id = row.getAttribute("id");
-            this.minutes = row.getElementsByClassName("minutes")[0]
-                .innerHTML;
-            this.seconds = row.getElementsByClassName("seconds")[0]
-                .innerHTML;
-            this.button = row.getElementsByClassName("go")[0]
-            this.going = false;
-            this.interval = null;
+            clearInterval(this.interval);
             
-            // Make sure time is displayed correctly
+            this.done += 1;
+            this.going = false;
+            this.button.removeAttribute("disabled");
+            this.minutes = 25;
+            this.seconds = 0;
             this.updateTime();
+            this.updateTally();
         }
-        
-        oneSecond() {
-            if (this.going == true) {
-                this.seconds -= 1;
-                if (this.seconds < 0) {
-                    this.minutes -= 1;
-                    this.seconds = 59;
-                }
-            }
+    }
+
+    updateTally() {
+        this.rowNum = this.done - 1;
+        if ((this.rowNum) % 4 == 0) {
+            console.log(Math.floor(this.rowNum / 4));
+            this.tally.insertRow(this.rowNum / 4);
         }
-        
-        checkIfDone() {
-            if (this.seconds == 0 && this.minutes == 0) {
-                document.title = "Tomate: Done!";
-                alert("Done!");
-                document.title = "Tomate"
-                this.going = false;
-                clearInterval(this.interval);
-                this.button.removeAttribute("disabled");
-                this.minutes = 25;
-                this.seconds = 0;
-                this.updateTime();
-            }
-        }
-        
-        updateTime() {
-            this.row.getElementsByClassName("minutes")[0].innerHTML 
-                = padShortNum(this.minutes);
-            this.row.getElementsByClassName("seconds")[0].innerHTML 
-                = padShortNum(this.seconds);
-            if (this.going == true) {
-                document.title = "Tomate: " + padShortNum(this.minutes) +
-                ":" + padShortNum(this.seconds);
-            }
-        }
-        
-        runTimer() {
-            if (this.going == true) {
-                this.oneSecond();
-                this.updateTime();
-                this.checkIfDone();
-            }
+        console.log(Math.floor(this.rowNum / 4));
+        this.tally.children[Math.floor(this.rowNum / 4)].innerHTML += "| ";
+    }
+    
+    updateTime() {
+        this.row.getElementsByClassName("minutes")[0].innerHTML 
+            = padShortNum(this.minutes);
+        this.row.getElementsByClassName("seconds")[0].innerHTML 
+            = padShortNum(this.seconds);
+        if (this.going == true) {
+            document.title = "Tomate: " + padShortNum(this.minutes) +
+            ":" + padShortNum(this.seconds);
         }
     }
     
-    var work = new Timer(document.getElementById("work"));
-    
-    function padShortNum(x) {
-        if (x < 10) {
-            return "0"+x;
-        } else {
-            return x;
+    runTimer() {
+        if (this.going == true) {
+            this.oneSecond();
+            this.updateTime();
+            this.checkIfDone();
         }
     }
+}   
     
-    function startTimer(o) {
-        o.going = true;
-        o.button.setAttribute("disabled", true);
-        o.interval = setInterval (
-            function() {o.runTimer() }, 1000
-        );
+var work = new Timer(document.getElementById("work"));
+
+function padShortNum(x) {
+    if (x < 10) {
+        return "0"+x;
+    } else {
+        return x;
     }
+}
+
+function startTimer(o) {
+    o.going = true;
+    o.button.setAttribute("disabled", true);
+    o.interval = setInterval (
+        function() {o.runTimer() }, 1000
+    );
+}
